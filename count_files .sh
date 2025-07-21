e79899cf2f9bca3e3c8f709e469e103b02527417
Asmah003 <moustaphaissoufouasmahyassafi@gmail.com>
2025-07-21 12:30:45 +0100

    asma

--- count_files .sh
+++ count_files .sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/bash # Cette ligne montre qu'on bash pour exécuter le script.
 
-read -p myfolder
-
+read -p myfolder 
  number=$(ls "$myfolder" | wc -l)
 
- count=$(echo "$number")
- 
+ count=$(echo "$number") 
+
  echo "Le dossier $myfolder contient $count fichier(s)."
