98d7d3836093e16a88f992ec078a6b5c94e0693e
Asmah003 <moustaphaissoufouasmahyassafi@gmail.com>
2025-07-21 12:50:06 +0100

    b

--- count_files .sh
+++ count_files .sh
@@ -1,9 +1,10 @@
 #!/bin/bash # cette ligne montre qu'on utilise bash pour excécuter le script.
-read -p "Entrez le nom du dossier: " dossier
+
+read -p "Entrez le nom du dossier : " dossier
 
     count=$(ls "$dossier" | wc -l)
 
     number=$(echo "$count") 
-    
+
     echo "Le dossier $dossier contient $number fichier(s)."
  
