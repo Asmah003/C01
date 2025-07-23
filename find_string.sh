d5b1962f300320a2ba080dd83691ebf01e290df2
Asmah003 <moustaphaissoufouasmahyassafi@gmail.com>
2025-07-23 15:11:48 +0100

    asmah

--- find_string.sh
+++ find_string.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 read fichier
+
 read chaine 
 
 if grep "$chaine" "$fichier"; then
+
     echo "La chaîne '$chaine' a été trouvée dans '$fichier'."
+
 else 
+
     echo "La chaîne '$chaine' n'a pas été trouvée dans '$fichier'."
+
 fi
