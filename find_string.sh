c9d93ca777a2146501a5520c5e262c74f31f3234
Asmah003 <moustaphaissoufouasmahyassafi@gmail.com>
2025-07-28 11:38:39 +0100

    asmah

--- find_string.sh
+++ find_string.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Demander le nom du fichier
-read -p "Entrez le nom du fichier : " fichier
+read fichier
 
-# Demander la chaîne à chercher
-read -p "Entrez la chaîne à rechercher : " chaine
+read chaine 
 
-# Vérifier si le fichier existe
-if [ ! -f "$fichier" ]; then
-    echo "Erreur : le fichier '$fichier' n'existe pas."
-    exit 1
-fi
+if grep "$chaine" "$fichier"; then
 
-# Recherche de la chaîne dans le fichier
-if grep -q "$chaine" "$fichier"; then
-    echo "La chaîne '$chaine' a été trouvée dans $fichier."
-else
-    echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
+    echo "La chaîne '$chaine' a été trouvée dans '$fichier'."
+
+else 
+
+    echo "La chaîne '$chaine' n'a pas été trouvée dans '$fichier'."
+
 fi
